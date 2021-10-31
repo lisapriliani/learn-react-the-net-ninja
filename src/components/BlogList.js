d4677192c55465d66fa2eadad71c392720e0e4c0
lisapriliani <lisaapriliani4@gmail.com>
2021-10-31 12:08:09 +0700

    props 2

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,19 @@
-const  = () => {
-    return (  
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>{blog.body}</p>
-              <p>{blog.author}</p>
-    );
-}
- 
-export default ;
+const BlogList = ({ blogs, title }) => {
+  // const blogs = props.blogs;
+  // const title = props.title;
+  // console.log(blogs);
+
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
